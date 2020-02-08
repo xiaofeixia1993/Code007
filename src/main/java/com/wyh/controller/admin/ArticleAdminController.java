89a22a4cf5390d697f9bb64a9216abfa3f5d583c
master <11221324@qq.com>
2020-02-08 13:39:26 +0800

    资源帖子审核实现

--- src/main/java/com/wyh/controller/admin/ArticleAdminController.java
+++ src/main/java/com/wyh/controller/admin/ArticleAdminController.java
@@ -2,14 +2,15 @@ package com.wyh.controller.admin;
 
 import com.wyh.Service.ArticleService;
 import com.wyh.entity.Article;
-import com.wyh.entity.Article;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
@@ -45,6 +46,45 @@ public class ArticleAdminController {
         resultMap.put("code", 0);
         resultMap.put("count", total);
         resultMap.put("data", articleList);
+        return resultMap;
+    }
+
+    /**
+     * 跳转到帖子审核页面
+     * @param id
+     * @return
+     */
+    @RequiresPermissions(value = {"跳转到帖子审核页面"})
+    @RequestMapping("/toReViewArticlePage/{id}")
+    public ModelAndView toReViewArticlePage(@PathVariable("id")Integer id) {
+        ModelAndView mav = new ModelAndView("admin/reviewArticle");
+        mav.addObject("title", "修改帖子页面");
+        mav.addObject("article", articleService.get(id));
+        return mav;
+    }
+
+    /**
+     * 修改状态
+     * @param article
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequiresPermissions(value = {"修改状态"})
+    @RequestMapping("/updateState")
+    public Map<String, Object> updateState(Article article) throws Exception {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        Article oldArticle = articleService.get(article.getId());
+        // TODO 添加一个消息模块
+        if (article.getState() == 2) {
+            oldArticle.setState(2);
+            //TODO 删除redis首页数据缓存
+        }else {
+            oldArticle.setState(3);
+            oldArticle.setReason(article.getReason());
+        }
+        articleService.save(oldArticle);
+        resultMap.put("success", true);
         return resultMap;
     }
 }
